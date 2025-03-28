fa01f8bdccd37666146ff206e6ffebc6bb6cafb7
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-03-28 15:44:32 +0545

    March 3

--- march3/script.js
+++ march3/script.js
@@ -23,3 +23,41 @@ const addSurname = arr.map((person) => ({
 console.log(addSurname);
 
 // Q.4 return only names starting with r, and declare him the winner, by adding new key winner as true
+const sortName = arr
+  .filter((person) => {
+    return person.name[0] == "r";
+  })
+  .map((item) => {
+    item.winner = true;
+    return item;
+  });
+
+console.log(sortName);
+
+const scores = [
+  ["math", 99],
+  ["science", 93],
+  ["computer", 95],
+];
+
+// calculate the percentage scored
+total = 0;
+scores.forEach((item) => {
+  total += item[1];
+});
+const percentage = (total / 300) * 100;
+console.log(total);
+console.log(percentage);
+
+// what is the lowest marks scored
+const scores = [
+    ["math", 99],
+    ["science", 93],
+    ["computer", 95],
+  ];
+  
+  // Find the lowest score using reduce
+  const lowestScore = scores.reduce((min, item) => Math.min(min, item[1]), Infinity);
+  
+  console.log("Lowest Marks Scored:", lowestScore); // Output: 93
+  
